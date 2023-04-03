a04b5bfff52e006620a5ee278d891bd6d7dce64e
V <v@gekron.com>
2023-04-03 17:52:22 -0400

    Initial commit

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="fixed w-full bg-white z-10 shadow">
+      <div className="py-4 border-b-[1px]">
+        <Container>fffff</Container>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="text-orange-500 text-3xl">hello airbnb</div>;
+}
