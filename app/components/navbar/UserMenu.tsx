992874d4d583688a76fdebe23037a90ee7f95738
V <v@gekron.com>
2023-04-03 23:41:27 -0400

    navbar updates

---
+++ app/components/navbar/UserMenu.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+const UserMenu = () => {
+  return (
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div
+          onClick={() => {}}
+          className="hidden md:block text-sm transition cursor-pointer font-semibold py-3 px-4 rounded-full hover:bg-neutral-100"
+        >
+          Airbnb you home
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserMenu;
